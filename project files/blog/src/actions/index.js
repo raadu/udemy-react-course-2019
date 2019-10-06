80ac90fab599bb88465a8a4361a9238a7f3a0c34
raadu <raiyad.raad@gmail.com>
2019-10-06 11:44:21 +0600

    chapter 14 finished, blog project updated with redux thunk

---
+++ project files/blog/src/actions/index.js
@@ -0,0 +1,17 @@
+import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+
+export const fetchPosts = () =>
+{
+	return async (dispatch) =>
+	{
+		const response = await jsonPlaceHolder.get('/posts');
+
+		dispatch(
+		{
+			type: 'FETCH_POSTS',
+			payload: response
+		})
+
+	};
+	
+};
